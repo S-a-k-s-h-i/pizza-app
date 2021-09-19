dd09fe6829f841ea79d8bc074cc21ee9908cd151
S-a-k-s-h-i <sakshipokhria1997@gmail.com>
2021-09-19 10:28:45 +0530

    'initial'

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 10,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard
+    },
+    AppService],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,32 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as helmet from 'helmet';
+import * as csurf from 'csurf';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  //to allow cross origin requests
+  app.enableCors();
+  
+  // app.use(csurf());
+  
+  //protect app from web vulnerabilities by setting http headers
+  app.use(helmet());
+  
+  //set static assets to public folder
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  //set views dir to views in resources folder
+  app.setBaseViewsDir(join(__dirname, '..', 'resources/views'));
+  //set Template engine
+  app.set('view engine', 'ejs');
+
+  await app.listen(3000);
+}
+bootstrap();
+function cookieParser(): any {
+  throw new Error('Function not implemented.');
+}
+
