2f17f31a15be07aa1c91fad550d56e850fb35310
S-a-k-s-h-i <sakshipokhria1997@gmail.com>
2022-05-29 16:32:04 +0530

    added user service class

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,20 @@
+import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+
+export class CreateUserDto{
+    @IsNotEmpty()
+    @IsEmail()
+    email:string;
+    
+    @IsNotEmpty()
+    @IsString()
+    username:string;
+    
+    @IsNotEmpty()
+    @IsString()
+    password:string;
+    
+    @IsNotEmpty()
+    @IsString()
+    address:string;
+
+}
--- src/user/service/user/user.service.ts
+++ src/user/service/user/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UserDocument } from 'src/schemas/user.schema';
+import { CreateUserDto } from 'src/user/dto/create-user.dto';
 
 @Injectable()
 export class UserService {
@@ -12,5 +13,9 @@ export class UserService {
      * @param createUserDto contains data to create user
      * @returns User
      */
-    async createUser(){}
+    async createUser(createUserDto:CreateUserDto):Promise<User>{
+        const user = new this.userModel(createUserDto);
+        return user.save();
+       
+    }
 }
